dc55d2b412af37c21d9fdfd82463edb174c795c2
Naveen <naveenkrishnawodeyar@gmail.com>
2023-12-24 23:42:48 +0530

    java.io.File

--- src/main/java/com/threads/file/CreatingFile.java
+++ src/main/java/com/threads/file/CreatingFile.java
@@ -1,15 +1,30 @@
 package com.threads.file;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class CreatingFile {
-	
-	public static void main(String[] args) throws IOException {
+	void createFile()
+	{
 		File f = new File("Test.txt");
-		if(f.createNewFile())
-			System.out.println("File created,");
-		else
-			System.out.println("Error in creating");
+		try {
+			if(f.createNewFile())
+				System.out.println("File created,"+f.getName());
+			else
+				System.out.println("Error in creating");
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.out.println(e.getLocalizedMessage());
+		}
 	}
+	
+	void writeToFile() throws IOException
+	{
+		FileWriter writer = new FileWriter("Test.txt");
+					writer.write("Welcome tot file operations in the Java,");
+					writer.close();
+		
+	}
+
 }
