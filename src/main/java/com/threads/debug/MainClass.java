77e61d2f68844d9cd6c422c5612613beb3ce7156
Naveen <naveenkrishnawodeyar@gmail.com>
2023-12-25 13:27:26 +0530

    scanner.nextInt()

--- src/main/java/com/threads/debug/MainClass.java
+++ src/main/java/com/threads/debug/MainClass.java
@@ -1,14 +1,23 @@
 package com.threads.debug;
 
+import java.util.Scanner;
+
 public class MainClass {
 
 	public static void main(String[] args) {
 		System.out.println("In Debug Mode,");
 		Calci calci = new Calci();
-				int add = calci.add(2, 2);
-				float prod = calci.prod(1, 1);
-		System.out.println("\nSum of given integers,"+add);
-		System.out.println("\nProduct of given integers,"+prod);
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter integer values:");
+		int a = scanner.nextInt();
+		int b = scanner.nextInt();
+				int add = calci.add(a,b);
+				float prod = calci.prod(a,b);
+				
+		if(add > prod)
+			System.out.println("\nSum of given integers,"+add);
+		else
+			System.out.println("\nProduct of given integers,"+prod);
 	}
 
 }
