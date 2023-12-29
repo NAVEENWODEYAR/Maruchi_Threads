c3b228b67574d28dfce56573f7b4139a42236bf6
Naveen <naveenkrishnawodeyar@gmail.com>
2023-12-29 21:38:35 +0530

    return super.equals(obj)

--- src/main/java/com/threads/object/Students.java
+++ src/main/java/com/threads/object/Students.java
@@ -2,18 +2,31 @@ package com.threads.object;
 
 public class Students {
 
+	private int stId;
+	private String stName;
+	
 	public Students() {	}
 	
-	public Students(int i,String s)
-	{
+	public Students(int i,String s){
 		this.stId = i;
 		this.stName = s;
 	}
 	
-
-	private int stId;
-	private String stName;
-	private String stMail;
-	private double stClass;
+	@Override
+	public boolean equals(Object obj) {
+		Students s = (Students) obj;
+		return super.equals(obj);
+	}
 	
+	public static void main(String[] args) {
+		Students s = new Students(0, null);
+		Students s1 = new Students(0, null);
+		
+		if(s.equals(s1))
+			System.out.println("equals()");
+		else if(s == s1)
+			System.out.println("== opearator,");
+		
+		System.out.println("***************");
+	}
 }
