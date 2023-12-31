76e68ff0e1c8fd2342c5baf197cba345f0799422
Naveen <naveenkrishnawodeyar@gmail.com>
2024-01-01 00:05:06 +0530

    Welcome 2024

--- src/main/java/com/last/SingleTonEx.java
+++ src/main/java/com/last/SingleTonEx.java
@@ -2,17 +2,19 @@ package com.last;
 
 public class SingleTonEx {
 
-	private SingleTonEx instance=null;
+	// Static variable reference of single_instance,
+	private static SingleTonEx instance=null;
 	
+	// private constructor,
 	private SingleTonEx(){
 		System.out.println("Private Constructor");
 	}
 	
-	public static SingleTonEx  getInstance()
+	// Static method to create instance of Singleton class
+	public static SingleTonEx getInstance()
 	{
-		if(instance==null) {
+		if(instance==null) 
 			instance= new SingleTonEx();
-		}
 		return instance;
 	}
 }
---
+++ src/main/java/com/last/SingleTonExImpl.java
@@ -0,0 +1,11 @@
+package com.last;
+
+public class SingleTonExImpl {
+
+	public static void main(String[] args) {
+		SingleTonEx s = SingleTonEx.getInstance();
+		SingleTonEx s1 = SingleTonEx.getInstance();
+		
+		System.out.println(s.hashCode()+" =Welcome **2024**= "+s1.hashCode());
+	}
+}
