5ac9beaf40fe10ee2e798323b7550a3d2abca24a
Naveen <naveenkrishnawodeyar@gmail.com>
2024-01-04 19:41:11 +0530

    SingleTon object

--- src/main/java/com/last/SingleTonExImpl.java
+++ src/main/java/com/last/SingleTonExImpl.java
@@ -5,6 +5,7 @@ public class SingleTonExImpl {
 	public static void main(String[] args) {
 		SingleTonEx s = SingleTonEx.getInstance();
 		SingleTonEx s1 = SingleTonEx.getInstance();
+		SingleTonEx s2 = SingleTonEx.getInstance();
 		
 		System.out.println(s.hashCode()+" =Welcome **2024**= "+s1.hashCode());
 	}
