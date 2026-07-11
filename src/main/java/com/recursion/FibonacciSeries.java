94952b5155f9c04ddc9d789bf2ebde5ad146c67b
Naveen Kumar K <108418552+NAVEENWODEYAR@users.noreply.github.com>
2026-07-11 21:47:13 +0530

    Update FibonacciSeries.java

--- src/main/java/com/recursion/FibonacciSeries.java
+++ src/main/java/com/recursion/FibonacciSeries.java
@@ -8,7 +8,7 @@ package com.recursion;
 public class FibonacciSeries {
 
 	public static void main(String[] args) {
-		System.out.println("\nFibonacci series using recursion\n");
+		System.out.println("\nFibonacci series demo using recursion\n");
 		int n = 20;
 		
 		for(int i=0; i<n; i++) {
